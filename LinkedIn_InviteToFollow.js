b7878f00b1953b99567fa5bf309a7d9a0a06328e
Joberto Lee <52058702+jobertol@users.noreply.github.com>
2020-01-29 14:00:18 -0800

    LinkedIn_InviteToFollow.js Initial Commit

---
+++ LinkedIn_InviteToFollow.js
@@ -0,0 +1,5 @@
+var butts = document.getElementsByClassName('org-invite-to-follow-modal-connection-card__cell-checkbox ember-checkbox ember-view');
+for(var i=0;i<butts.length;i++)
+  {
+    butts[i].click();
+  }
